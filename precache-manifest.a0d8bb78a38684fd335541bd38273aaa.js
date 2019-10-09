b69d42f0049a70d372d35274533391f8f9746cbf
Abhik Rai <a00258752@student.ait.ie>
2019-10-09 18:38:10 +0100

    Updates

---
+++ precache-manifest.a0d8bb78a38684fd335541bd38273aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb3da06170caf722a568",
+    "url": "/MyWebsite/static/js/runtime~main.cb3da061.js"
+  },
+  {
+    "revision": "6a958c62874172069fef",
+    "url": "/MyWebsite/static/js/main.6a958c62.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyWebsite/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6a958c62874172069fef",
+    "url": "/MyWebsite/static/css/main.2288a531.chunk.css"
+  },
+  {
+    "revision": "0d8742db6e79ce0900d3df9b43e75f6d",
+    "url": "/MyWebsite/index.html"
+  }
+];
